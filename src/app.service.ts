2c3133eee95be0caff243f32eabdc3189a66fef2
Joao Oliveira <joaoo.rocha1@gmail.com>
2023-11-10 23:06:24 -0300

    fdsfsd

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World PRODUÇÃO!';
+    return 'Hello World PRODUÇÃO RENAN!';
   }
 }
